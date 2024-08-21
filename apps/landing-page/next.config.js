308b6942ae77724d023682ab2dd55bd395de3290
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-08-21 21:09:14 +0300

    init

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -3,16 +3,32 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig =
+  withBundleAnalyzer({
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
+                       eslint: {
+                         dirs: ['.'],
+                       },
+                       poweredByHeader: false,
+                       trailingSlash: true,
+                       basePath: '',
+                       // The starter code load resources from `public` folder with `router.basePath` in React components.
+                       // So, the source code is "basePath-ready".
+                       // You can remove `basePath` if you don't need it.
+                       reactStrictMode: true,
+});
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
